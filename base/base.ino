3ec411b65c9c36909313765c2ccb3a2bc9238c94
Protodrew <pull.rebase>
2021-10-09 17:06:36 -0400

    refactor, v1

--- base/base.ino
+++ base/base.ino
@@ -1,82 +1,50 @@
-int sol1 = 2;
-int sol2 = 4;
-int sol3 = 6;
-int sol4 = 8;
-int mot = 10;
+#include "guidarduino.h" 
 
-struct Note{
-     int solenoid; // 1-4 (will be multiplied by 2 when outputting to pin)
-     int string; // 1-6
-
-};
-
-struct Pentatonic{
-    char key;
-    int keyType; // major = 1, minor =  0
-    Note notes[5];
-};
-  
+int solenoid[4] = {4,5,6,7}; // solenoid pins
+int actuator[2] = {2,3};
+int scale1[6][4] = {{1,1,0,1},{1,0,1,1},{1,0,1,1},{1,0,1,0},{1,1,0,1},{1,1,0,1}};
+int strlength = 50;
 void setup() {
-  pinMode(sol1, OUTPUT);
-  pinMode(sol2, OUTPUT);
-  pinMode(sol3, OUTPUT);
-  pinMode(sol4, OUTPUT);
-  Serial.begin(9600);
+  pinMode(solenoid[0], OUTPUT);
+  pinMode(solenoid[1], OUTPUT);
+  pinMode(solenoid[2], OUTPUT);
+  pinMode(solenoid[3], OUTPUT);
+  pinMode(actuator[0], OUTPUT);
+  pinMode(actuator[1], OUTPUT);
   question();
 }
 
 void loop(){
+    
+    for(int x = 0; x < 6; x++){
+      actu_control(1)
+      delay(strlength);
+      actu_control(0)
+      for(int s = 0; s < 4; s++){
+        if(scale1[x][s] == 1){
+        digitalWrite(solenoid[s+4],HIGH);
+        digitalWrite(solenoid[(s+4)-1],LOW);
+        delay(100);
+        }
+       
+      }
+    }
+    actu_control(-1);
+    delay(2000);
 
-  while (Serial.available() == 0) {
-    // Wait for User to Input Data
+}
+
+void actu_control(int dir){
+  if(dir==0) {
+    digitalWrite(actuator[0], LOW);
+    digitalWrite(actuator[1], LOW);
   }
-  String temp = Serial.readString();
-  char *input;
-  temp.toCharArray(input,50);
-  char inpKey = strtok(input,",");
-  char inp2 = strtok(input,","); // temporary, for validation and conversion
-  char pent = strtok(NULL,",");
-  int inpKeyType = validate(inp2);
-  int isPenta = 1; // will eventually be mutable if pentatonic playing goes smoothly and we have more time
-
-  struct Pentatonic toPlay;
-  toPlay.key = inpKey;
-  toPlay.keyType = inpKeyType; 
-  //Notes will be filled in seperate function
-  
-  
-
-
-}
-
-int validate(char temp){
-  switch(temp){ // TODO, lowercase conversion for cleaner code
-    case 'y':
-      return 1;
-    case 'Y':
-      return 1;
-    case 'n':
-      return 0;
-     case 'N':
-      return 0;
-    default:
-      Serial.println("INVALID KEY TYPE, defaulting to major and pentatonic");
-      return 1;
+  if(dir<0) {
+    digitalWrite(actuator[0], HIGH);
+    digitalWrite(actuator[1], LOW);
   }
-}
-
-void play(Pentatonic scale){
-  /* TODO organize solanoid input
-   *  via a switch...case
-   */
-}
-
-void fillNotes(struct pentatonic *scale){
-  /*
-   * TODO, write a looped switch...case to fill struct's Note array
-   */
-}
-
-void question(){
-  Serial.println("what scale are we playing? (seperate with commas)\n+-----------+--------+-------------+\n|    Key    | Major? | Pentatonic? |\n+-----------+--------+-------------+\n| c,d,e...  | Y/n    | Y/n         |\n+-----------+--------+-------------+");
+  if(dir>0) {
+    digitalWrite(actuator[0], LOW);
+    digitalWrite(actuator[1], HIGH);
+  }
 }
