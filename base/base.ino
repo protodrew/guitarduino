0ce2a919be24605f55ee11e956f1942f1ff0d91d
Protodrew <pull.rebase>
2021-10-10 15:48:59 -0400

    refactor, v1

--- base/base.ino
+++ base/base.ino
@@ -1,9 +1,7 @@
-#include "guidarduino.h" 
-
 int solenoid[4] = {4,5,6,7}; // solenoid pins
 int actuator[2] = {2,3};
 int scale1[6][4] = {{1,1,0,1},{1,0,1,1},{1,0,1,1},{1,0,1,0},{1,1,0,1},{1,1,0,1}};
-int strlength = 50;
+int strlength = 423;
 void setup() {
   pinMode(solenoid[0], OUTPUT);
   pinMode(solenoid[1], OUTPUT);
@@ -11,26 +9,33 @@ void setup() {
   pinMode(solenoid[3], OUTPUT);
   pinMode(actuator[0], OUTPUT);
   pinMode(actuator[1], OUTPUT);
-  question();
 }
 
 void loop(){
     
     for(int x = 0; x < 6; x++){
-      actu_control(1)
+      actu_control(1);
       delay(strlength);
-      actu_control(0)
+      actu_control(0);
       for(int s = 0; s < 4; s++){
+        
         if(scale1[x][s] == 1){
-        digitalWrite(solenoid[s+4],HIGH);
-        digitalWrite(solenoid[(s+4)-1],LOW);
-        delay(100);
+        digitalWrite(solenoid[s],HIGH);
+         
+        delay(150);
+        digitalWrite(solenoid[s],LOW);
+        
+        delay(150);
         }
+        
+        
        
       }
+      
+      
     }
     actu_control(-1);
-    delay(2000);
+    delay(2200);
     actu_control(0);
 }
 
